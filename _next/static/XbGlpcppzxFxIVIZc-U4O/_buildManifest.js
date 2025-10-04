db27e11fbf5d06054fdfd7fac5b9cd9754bfbbe0
Mubeen S <mubeen.shaik@advance-auto.com>
2025-10-04 17:55:00 +0530

    Updates

---
+++ _next/static/XbGlpcppzxFxIVIZc-U4O/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/6c28b961c7526115.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mmdb-home//_next/:path+",
+        "destination": "/mmdb-home/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/04fe286640e22d5c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/1bd58b1074f445a5.js",
+  "static/chunks/turbopack-c5a3e425b05b20d0.js"
+])
